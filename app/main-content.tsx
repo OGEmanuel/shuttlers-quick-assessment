53a8a13ef34e6baaf18c535502a3e231dbce6f01
OGEmanuel <emanuolumide@gmail.com>
2023-10-22 22:10:32 +0100

    Graph almost done

--- app/main-content.tsx
+++ app/main-content.tsx
@@ -16,9 +16,10 @@ import {
   CategoryScale,
   LinearScale,
   PointElement,
+  Filler,
 } from "chart.js";
 
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
+ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Filler);
 
 const MainContent = () => {
   const data = {
@@ -35,27 +36,20 @@ const MainContent = () => {
     ],
     datasets: [
       {
-        labels: "",
-        data: [0, null, null, 3, null, null, 5, null, 1],
-        backgroundColor: "#1FAD32",
+        label: "Data",
+        data: [0, null, null, 2.5, null, null, 3, null, 0.5],
+        backgroundColor: "rgb(229, 250, 232, 0.5)",
         borderColor: "#1FAD32",
         pointBorderColor: "#1FAD32",
+        pointBackgroundColor: "#1FAD32",
         borderWidth: 1,
         fill: true,
         spanGaps: true,
-        segment: {
-          backgroundColor: "#1FAD32",
-        },
       },
     ],
   };
 
   const options = {
-    // layout: {
-    //   padding: {
-    //     top: 2,
-    //   },
-    // },
     maintainAspectRatio: false,
     scales: {
       x: {
@@ -71,10 +65,13 @@ const MainContent = () => {
           display: false,
         },
         min: 0,
-        max: 6,
+        max: 3,
         ticks: {
           display: false,
           stepSize: 0.5,
+        },
+        grid: {
+          color: "#F7F7F7",
         },
       },
     },
@@ -121,25 +118,46 @@ const MainContent = () => {
         </div>
       </div>
 
-      <div>
+      <div className="w-full">
         <p className="mb-10 text-[#060606] text-xl">Dashboard Overview</p>
 
-        <div className="p-10 border-[#E6E6E6] border-[1.215px] rounded-[9.717px] shadow-[0px_0px_29.15122px_0px_rgba(235,235,235,0.25)]">
-          <div className="flex justify-between items-start border-b-[#E6E6E6] border-b-[1.215px] pb-6 mb-16">
-            <div className="flex flex-col gap-6">
-              <p className="text-lg text-[#666666] font-medium">
-                Vouchers Issued
-              </p>
-              <p className="font-bold text-[2rem]">192</p>
+        <div className="flex gap-4 w-full">
+          <div className="p-10 border-[#E6E6E6] border-[1.215px] rounded-[9.717px] shadow-[0px_0px_29.15122px_0px_rgba(235,235,235,0.25)] w-full">
+            <div className="flex justify-between items-start border-b-[#E6E6E6] border-b-[1.215px] pb-6 mb-16">
+              <div className="flex flex-col gap-6">
+                <p className="text-lg text-[#666666] font-medium">
+                  Vouchers Issued
+                </p>
+                <p className="font-bold text-[2rem]">192</p>
+              </div>
+              <div className="flex items-center gap-3 px-4 py-[0.85rem] border-[1.767px] border-[#E8E8E8] rounded-[16.154px] cursor-pointer hover:bg-[#F4F4F4]">
+                <CalendarIcon />
+                <p className="text-[#101010] font-bold text-sm">Last 30 Days</p>
+                <CaretDown />
+              </div>
             </div>
-            <div className="flex items-center gap-3 px-4 py-[0.85rem] border-[1.767px] border-[#E8E8E8] rounded-[16.154px] cursor-pointer hover:bg-[#F4F4F4]">
-              <CalendarIcon />
-              <p className="text-[#101010] font-bold text-sm">Last 30 Days</p>
-              <CaretDown />
+            <div className="h-[233px]">
+              <Line data={data} options={options}></Line>
             </div>
           </div>
-          <div className="h-[233px]">
-            <Line data={data} options={options}></Line>
+
+          <div className="p-10 border-[#E6E6E6] border-[1.215px] rounded-[9.717px] shadow-[0px_0px_29.15122px_0px_rgba(235,235,235,0.25)] w-full">
+            <div className="flex justify-between items-start border-b-[#E6E6E6] border-b-[1.215px] pb-6 mb-16">
+              <div className="flex flex-col gap-6">
+                <p className="text-lg text-[#666666] font-medium">
+                  Vouchers Issued
+                </p>
+                <p className="font-bold text-[2rem]">200</p>
+              </div>
+              <div className="flex items-center gap-3 px-4 py-[0.85rem] border-[1.767px] border-[#E8E8E8] rounded-[16.154px] cursor-pointer hover:bg-[#F4F4F4]">
+                <CalendarIcon />
+                <p className="text-[#101010] font-bold text-sm">Last 30 Days</p>
+                <CaretDown />
+              </div>
+            </div>
+            <div className="h-[233px]">
+              <Line data={data} options={options}></Line>
+            </div>
           </div>
         </div>
       </div>
